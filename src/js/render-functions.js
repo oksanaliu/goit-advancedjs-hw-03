1a314b5528efb1415a3fa15a1d2980c131e03fa6
oksanaliu <lyuklyan4@icloud.com>
2024-12-17 22:10:47 +0100

    c

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -43,7 +43,7 @@ export function showError(message) {
   iziToast.error({
     title: 'Error',
     message,
-    position: 'topCenter',
+    position: 'topRight',
   });
 }
 
@@ -51,6 +51,6 @@ export function showInfo(message) {
   iziToast.info({
     title: 'Info',
     message,
-    position: 'topCenter',
+    position: 'topRight',
   });
 }
